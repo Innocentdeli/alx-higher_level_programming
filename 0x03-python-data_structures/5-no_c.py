09502217c134c182a49605d44b03819b07becd0d
Innocent Deli <innocentdeli1@gmail.com>
2024-01-06 14:31:16 -0800

    Data structure

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in my_string:
-        count = 0
-        if i == "c" or i == "C":
-            my_string.remove(count, i)
-            count += 1
-    return my_string
+    new_string = ""
+    for elements in my_string:
+        if elements != "c" and elements != "C":
+            new_string += elements
+    return new_string
