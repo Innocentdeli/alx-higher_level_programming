613a3f467c8d1126b8371d8e6c1a70c8ee2375e9
Innocent Deli <innocentdeli1@gmail.com>
2024-01-07 13:28:36 -0800

    Data structure

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if my_list:
+        big = 0
+        for i in my_list:
+            if i > big:
+                big = i
+        return big
+    else:
+        return "None"
