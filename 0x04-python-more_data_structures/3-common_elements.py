815477700fae967c7a4cd05becd033cfa56e62bd
Innocent Deli <innocentdeli1@gmail.com>
2024-01-12 01:50:55 -0800

    More data structure

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    new_set = []
+    for i in set1:
+        for j in set2:
+            if i == j:
+                new_set.append(i)
+    return new_set
